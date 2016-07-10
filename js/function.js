f6ad5ac9c80aa475f3eeaaad475a9b4eacb6da89
wangsongiam <wangsongiam@gmail.com>
2016-07-10 20:48:02 +0800

    loop inside function

--- js/function.js
+++ js/function.js
@@ -10,3 +10,14 @@ var writeAName = function(name) {
 }
 
 writeAName('wangsong');
+
+
+var writeNameManyTimes = function (name) {
+	var i;
+	
+	for (i = 0; i < 10; i++) {
+	document.write(name);
+}
+};
+
+writeNameManyTimes('T-Rex');
