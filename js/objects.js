b4e4fa7c4eaf5bc44eb0930e3e0afd59af7daf8a
wangsongiam <wangsongiam@gmail.com>
2016-07-11 00:06:46 +0800

    object
    an object is a collection of variables!!!
    
    iguanodon.diet   is a variable
    iguanodon  is a object

---
+++ js/objects.js
@@ -0,0 +1,11 @@
+var iguanodon = {
+	name: 'Iguanodon',
+	height: '13m',
+	weight: '3t',
+};
+
+console.log(iguanodon.name);
+console.log(iguanodon.weight);
+
+iguanodon.diet = 'Herbivore';
+console.log(iguanodon);
