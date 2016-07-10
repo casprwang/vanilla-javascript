a9d3d8ed45717c7576d69810c6c256dc02ea5808
wangsongiam <wangsongiam@gmail.com>
2016-07-11 00:17:47 +0800

    objection is a array

--- js/objects.js
+++ js/objects.js
@@ -9,3 +9,26 @@ console.log(iguanodon.weight);
 
 iguanodon.diet = 'Herbivore';
 console.log(iguanodon);
+
+var spinosaurus = {
+	name: 'Spinosaurus',
+	height: '15m',
+	weight: '6t',
+	diet: 'Carnivore'
+};
+
+var microraptor = {
+	name: 'Microraptor',
+	height: '80cm',
+	weight: '1kg',
+	diet: 'Carnivore'
+};
+
+
+var dinos = [iguanodon, spinosaurus, microraptor];
+
+dinos.forEach(function(item) {
+	document.write('<h2>' + item.name + '</h2>');
+});
+	
+
