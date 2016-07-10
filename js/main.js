fd86952a51cb082ff2b458cd0c87be9c839a9386
wangsongiam <wangsongiam@gmail.com>
2016-07-10 19:52:09 +0800

    if (true)

--- js/main.js
+++ js/main.js
@@ -10,4 +10,14 @@ var num = 65.4312;
 // alert(age % num);
 
 var newNum = num - age;
-alert(newNum);
+// alert(newNum);
+
+var isHuman = true;
+var isDinosaur = false;
+
+
+if (true) {
+	alert('This is true')
+}	else {
+	alert('This is false')
+}
