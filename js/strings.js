c8418dfe7fc1a0ef01f0812b2927950112395064
wangsongiam <wangsongiam@gmail.com>
2016-07-11 14:47:57 +0800

    play with strings

---
+++ js/strings.js
@@ -0,0 +1,18 @@
+var name = 'Wang Song';
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase());
+
+// letter in the string
+console.log(name.slice(3, 6));
+
+console.log(name.substr(3, 6));
+
+// length
+console.log(name.length);
+
+console.log(name.replace('a', 'o'));
+
+var nameBits = name.split(' ');
+console.log(nameBits);
