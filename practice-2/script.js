9196bdc7dbcd15c491f18fca6035e35269d0dd5a
wangsongiam <wangsongiam@gmail.com>
2016-08-29 19:53:01 -0400

    add new

---
+++ practice-2/script.js
@@ -0,0 +1,10 @@
+var balance = 500000;
+
+if (balance >=0 ) {
+	alert("The balance is positive");
+	if (balance > 10000) {
+		alert("The balance is large!");
+	}
+} else {
+	alert("The balance if negative");
+}
