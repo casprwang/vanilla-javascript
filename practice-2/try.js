6c6731341b3c124420f3c1a75fedf1bf322b54cb
wangsongiam <wangsongiam@gmail.com>
2017-01-06 16:01:28 -0500

    📝

---
+++ practice-2/try.js
@@ -0,0 +1,37 @@
+console.log('work')
+
+
+var aaaa = 'hello';
+
+var aaaaa
+
+function bbbbb  (){
+  console.log('worl')
+}
+
+
+// person.set('name', 'Song');
+
+const person = new Map([
+  ['name', 'Song'],
+  ['age', '31']
+]);
+
+// person.set('name', 'Song');
+// person.set('age', '31');
+
+// console.log(person.get('age'));
+// console.log(person.size);
+
+
+// // forEach is available for set map and array
+// person.forEach((val, key) => {
+//   console.log(val, key);
+// });
+
+// console.log(person.keys());
+
+console.log(person.entries());
+// const stupid = ()=>{
+//   console.log('stupid')
+// }
