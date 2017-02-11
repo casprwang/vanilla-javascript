fd2287d4c3314ac1f62a464cdc6f45253fb86155
wangsongiam <wangsongiam@gmail.com>
2017-02-11 16:31:49 -0500

    onclick function

---
+++ test/script.js
@@ -0,0 +1,10 @@
+function myFunction () {
+  let x
+  if (confirm('Press a button!') == true) {
+    x = 'You pressed OK!'
+  } else {
+    x = 'You pressed Cancel!'
+  }
+  // document.getElementsByTagName('p').innerHTML = x
+  document.getElementById('de').innerHTML = x
+}
